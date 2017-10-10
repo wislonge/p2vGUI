88e16e8a611df1672681445e5546c4fc5e4c2d70
william <william@localhost.localdomain>
2017-10-10 02:03:18 -0400

    add fedora gen shell

---
+++ fedora25-livecd.sh
@@ -0,0 +1,3 @@
+./run p2v/virt-p2v-make-kickstart https://dl.fedoraproject.org/pub/fedora/linux/releases/25/Everything/x86_64/os/ --install kde-i18n,kde-i18n-Chinese,kde-l10n,kde-l10n-zh_CN,glibc-common,glibc-langpack-zh,cjkuni-uming-fonts
+
+livecd-creator ./p2v.ks
